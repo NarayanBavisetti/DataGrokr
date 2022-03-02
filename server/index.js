898db054db3aac1315b8287076b80bd9b8a517f5
narayanbavisetti <narayan3119@gmail.com>
2022-03-03 02:35:49 +0530

    working functionality done

--- server/index.js
+++ server/index.js
@@ -1,24 +1,32 @@
-const express = require('express');
+const express = require("express");
+const bodyparser = require("body-parser");
 const app = express();
 const PORT = 5000;
-const mysql = require('mysql');
+const mysql = require("mysql");
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyparser.urlencoded({ extended: true }));
 
 const db = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "12345678",
-    database:"new_schema",
-})
+  host: "localhost",
+  user: "root",
+  password: "12345678",
+  database: "new_schema",
+});
 
-
-app.get("/",(req,res) => {
-    const sqlInsert = "INSERT INTO userinfo (first_name, last_name, email_id ,mobile_no , account_no, ifsc_code) VALUES ('bavi' , 'setti' , 'nara@g.com', 554,2321,314134);";
-    db.query(sqlInsert , (err,result ) => {
-        res.send("kuch bhi");
-    })
-
-})
+app.post("/api/insert", (req, res) => {
+  const { first_name, last_name, email_id, mobile_no, account_no, ifsc_code } = req.body;
+  const sqlInsert = "INSERT INTO userinfo (first_name, last_name, email_id ,mobile_no , account_no, ifsc_code) VALUES (?,?,?,?,?,?)";
+  db.query( sqlInsert, [first_name, last_name, email_id, mobile_no, account_no, ifsc_code],
+    (err, result) => {
+      res.send("Hello");
+      console.log("added successfully");
+    }
+  );
+});
 
 app.listen(PORT, () => {
-    console.log(`server is running at ${PORT}`);
-})
+  console.log(`server is running at ${PORT}`);
+});
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useState, useEffect } from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
@@ -6,8 +6,67 @@ import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
+import Axios from "axios";
+
+const getDataFromLocalStorage = () => {
+  const value = localStorage.getItem("data");
+  if (value) {
+    return JSON.parse(value);
+  }
+};
 
 export default function App() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [mobileNumber, setMobileNumber] = useState();
+  const [accountNumber, setAccountNumber] = useState();
+  const [ifscCode, setIfscCode] = useState();
+  const [mcq, setMcq] = useState();
+
+  const [data, setData] = useState(getDataFromLocalStorage());
+
+  const submitData = (e) => {
+    e.preventDefault();
+    if (
+      (!firstName || !lastName,
+      !mobileNumber,
+      !email,
+      !accountNumber,
+      !ifscCode,
+      !mcq)
+    ) {
+      alert("please fill all the details");
+    } else {
+      if (mcq == "LocalStorage") {
+        let givendata = {
+          firstName,
+          lastName,
+          mobileNumber,
+          email,
+          accountNumber,
+          ifscCode,
+        };
+        setData([...data, givendata]);
+      } else {
+        Axios.post("http://localhost:5000/api/insert", {
+          first_name: firstName,
+          last_name: lastName,
+          email_id: email,
+          mobile_no: mobileNumber,
+          account_no: accountNumber,
+          ifsc_code: ifscCode,
+        }).then(() => {
+          alert("successful insert");
+        });
+      }
+    }
+  };
+
+  useEffect(() => {
+    localStorage.setItem("data", JSON.stringify(data));
+  }, [data]);
+
   return (
     <Box
       component="form"
@@ -18,16 +77,54 @@ export default function App() {
       autoComplete="off"
     >
       <div>
-        <TextField id="outlined" type="text" label="First Name" />
-        <TextField id="outlined" type="text" label="Last Name" />
+        <TextField
+          id="outlined"
+          type="text"
+          label="First Name"
+          onChange={(e) => setFirstName(e.target.value)}
+          value={firstName}
+        />
+        <TextField
+          id="outlined"
+          type="text"
+          label="Last Name"
+          onChange={(e) => setLastName(e.target.value)}
+          value={lastName}
+        />
       </div>
       <div>
-        <TextField id="outlined" type="email" label="Email ID" />{" "}
-        <TextField id="outlined" label="Mobile No." />
+        <TextField
+          id="outlined"
+          type="email"
+          label="Email ID"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />{" "}
+        <TextField
+          id="outlined"
+          type="number"
+          label="Mobile No."
+          onChange={(e) => setMobileNumber(e.target.value)}
+          value={mobileNumber}
+        />
       </div>
       <div>
-        <TextField id="outlined" inputmode="numeric" label="Account No." />{" "}
-        <TextField id="outlined" inputmode="numeric" label="IFSC Code" />
+        <TextField
+          id="outlined"
+          type="number"
+          inputmode="numeric"
+          label="Account No."
+          onChange={(e) => setAccountNumber(e.target.value)}
+          value={accountNumber}
+        />{" "}
+        <TextField
+          id="outlined"
+          type="number"
+          inputmode="numeric"
+          label="IFSC Code"
+          onChange={(e) => setIfscCode(e.target.value)}
+          value={ifscCode}
+        />
       </div>
       <div>
         <FormControl sx={{ m: 1, minWidth: 120 }}>
@@ -38,9 +135,8 @@ export default function App() {
             labelId="demo-simple-select-helper-label"
             id="demo-simple-select-helper"
             label="Storage Medium"
-            // value={age}
-
-            // onChange={handleChange}
+            value={mcq}
+            onChange={(e) => setMcq(e.target.value)}
           >
             <MenuItem value="LocalStorage">LocalStorage</MenuItem>
             <MenuItem value="Database">Database</MenuItem>
@@ -48,7 +144,7 @@ export default function App() {
         </FormControl>
       </div>
       <div>
-        <Button variant="contained" disabled>
+        <Button onClick={submitData} variant="contained">
           {" "}
           Submit{" "}
         </Button>
